207139bff8ead8de53351158f79b5d9d897028cf
ahmedadel101 <a.adel9@outlook.com>
2020-05-07 15:49:20 -0700

    add file in html

--- postsManage.js
+++ postsManage.js
@@ -1,13 +1,12 @@
+
+$(window).on('load', function(){ 
+
 let postman = document.getElementById('postMan');
-let btnEdit = document.getElementById('edit');
-let confirmDel = document.getElementById('confirmDelete');
-
-
-
 let postManage = [];
 
 postManage = JSON.parse(localStorage.getItem('postsData'));
 
+postman.addEventListener('click', editbtns);
 AddPosts();
 
 function AddPosts() {
@@ -19,8 +18,8 @@ function AddPosts() {
                 <p>${postManage[i].body}</p>
                 </div>
                 <div class=" d-flex mt-2 justify-content-center align-items-end">
-                <a id="edit" data_index="${i}" data_id="${postManage[i].id}" class="btn text-danger mr-3 btn-warning " >Edit</a>
-                <a  onmouseover"dell()"  data_index="${i}" data_id="${postManage[i].id}" class=" dell btn btn-danger text-warning" data-toggle="modal" data-target="#warningModal">Delete</a>
+                <a  data_index="${i}" data_id="${postManage[i].id}" class="editbtn btn text-danger mr-3 btn-warning " >Edit</a>
+                <a data_index="${i}" data_id="${postManage[i].id}"  class=" delbtn btn btn-danger text-warning data-toggle="modal" data-target="#exampleModalCenter" >Delete</a>
                 </div>
                 </div>`
                 postman.innerHTML = temp;
@@ -28,17 +27,56 @@ function AddPosts() {
     }   
 }
 
-function dell(){
-    $('#warningModal').modal('hide');
-    console.log()
-}
-       
 
-       
+function editbtns (e) {
 
+    let postId;
+    let postIndex;
 
-        
+            if (e.target.classList.contains('delbtn')) {
+                postId = e.target.getAttribute('data_id');
+                postIndex = e.target.getAttribute('data_index');
+                let confirmDelete = document.getElementById('confirm');
+                $('#exampleModalCenter').modal('show');
+
+                confirmDelete.addEventListener('click', function (){
+
+                    postManage.splice(postIndex, 1);
+                    localStorage["postManage"] = (JSON.stringify(postManage));
+
+                    AddPosts();
+                    $('#exampleModalCenter').modal('hide');
+
+                    let delRequest = new Promise ((resolve,reject) => {
+                        let request = new XMLHttpRequest;
+                        request.open('DELETE', `https://jsonplaceholder.typicode.com/posts/${postId}`);
+                        request.send();
+                        request.onload = () => {
+                            if (request.status === 200) {
+                                resolve(Response);
+                                console.log(request.response);
+                            }
+                            else if (request.status === 404){
+                                let error = 'No posts DELETED'
+                                reject(error);
+                            }
+                        }
+                    });
+    
+                    delRequest.then((message) => {
+                        console.log(message);
+                    }, (error) => {
+                        console.log(error);
+                    });
+                });
+            }
+            else if (e.target.classList.contains('editbtn')) {
+                postId = e.target.getAttribute('data_id');
+                window.location.href = './edit_post.html?'+postId
+
+            }
    
 
-    
+        }
 
+    });
